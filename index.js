ebac98dc05b10771f119a613d09caa5b13a955a6
David Glivar <davidglivar@gmail.com>
2014-09-09 09:43:54 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var voyager = require('voyager')
+  , browserify = require('browserify')
+  , vfs = require('vinyl-fs')
+  , vss = require('vinyl-source-stream');
+
+voyager.task('browserify', function (done) {
+  browserify({ debug: true })
+    .add(voyager.SRC + '/javascripts/main.js')
+    .bundle()
+    .pipe(vss('main.js'))
+    .pipe(vfs.dest(voyager.TMP + '/javascripts'))
+    .on('end', done);
+});
