2cfcfdbab8b0a546aa670219e15137df9dfd9cc3
David Glivar <davidglivar@gmail.com>
2014-09-12 16:47:48 -0700

    updated to match latest voyager feature

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ var voyager = require('voyager')
   , vfs = require('vinyl-fs')
   , vss = require('vinyl-source-stream');
 
-voyager.task('browserify', 'scripts', function (done) {
+voyager.task('scripts-prebuild', ['scripts', 'prebuild'], function (done) {
   browserify({ debug: true })
     .add(voyager.SRC + '/javascripts/main.js')
     .bundle()
     .pipe(vss('main.js'))
     .pipe(vfs.dest(voyager.TMP + '/javascripts'))
+    .on('end', done);
+});
+
+voyager.task('scripts-vendor', ['scripts', 'prebuild'], function (done) {
+  vfs.src(this.SRC + '/javascripts/vendor/**/*.js')
+    .pipe(vfs.dest(this.TMP + '/javascripts/vendor'))
+    .on('end', done);
+});
+
+voyager.task('scripts-build', ['scripts', 'build'], function (done) {
+  vfs.src([
+      this.TMP + '/javascripts/main.js'
+    , '!' + this.TMP + '/javascripts/vendor/*'
+    ])
+    .pipe(vfs.dest(this.BLD + '/javascripts'))
+    .on('end', done);
+});
+
+voyager.task('scripts-build-vendor', ['scripts', 'build'], function (done) {
+  vfs.src(this.TMP + '/javascripts/vendor/**/*.js')
+    .pipe(vfs.dest(this.BLD + '/javascripts/vendor'))
     .on('end', done);
 });
